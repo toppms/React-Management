9fbd59161cb51ff975d0fc824ea50be159b3a3bc
toppms <top1604@naver.com>
2021-06-23 09:57:16 +0900

    Create Custoer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [{
+  'id' :1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '박민석',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}, 
+
+{
+  'id' :2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '970302',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+
+{
+  'id' :3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '이민정',
+  'birthday' : '960723',
+  'gender' : '여자',
+  'job' : '편집자'
+}]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map (c => {
+            return (
+              <Customer
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+              />
+            );
+          })
+        }
+      
+      </div>
+    );
+  }
 }
 
 export default App;
