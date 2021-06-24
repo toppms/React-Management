e0c91e451ae23a2f01374d7b7a7b1c75b276df84
toppms <top1604@naver.com>
2021-06-24 16:29:50 +0900

    Apply Material-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import { Component } from 'react';
 import './App.css';
-import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper';
+import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
 
+const styles = theme => ({
+  root: {
+    with: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX : "auto"
+  },
+  table: {
+    minWidth: 1000
+  }
+})
 const customers = [{
   'id' :1,
   'image' : 'https://placeimg.com/64/64/1',
@@ -31,9 +48,22 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
           customers.map (c => {
             return (
               <Customer
@@ -48,10 +78,13 @@ class App extends Component {
             );
           })
         }
+          </TableBody>
+        </Table>
+    
       
-      </div>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
